0b812eb39d35fee685123f599c3b5b39c375beb7
Kaiohshin <47540180+Kaiohshin@users.noreply.github.com>
2025-01-22 15:18:35 +0200

    AMI

--- compute.tf
+++ compute.tf
@@ -1,13 +1,24 @@
 # Docker Instance
 resource "aws_instance" "docker_instance" {
-  ami                    = "ami-053a862cc72bed182"
-  instance_type          = var.docker_instance
-  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+    most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amazon/amzn2-ami-kernel-5.10-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
 
   #User Data in AWS EC2
   user_data = file("docker_install.sh")
 
   tags = {
-    Name = "Docker Python Flask"
+    Name = "Docker"
   }
 }
