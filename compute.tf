b99671682f5c05747f75a628576917a08b57291a
Kaiohshin <47540180+Kaiohshin@users.noreply.github.com>
2025-01-22 15:33:42 +0200

    test ami

--- compute.tf
+++ compute.tf
@@ -1,6 +1,6 @@
 # Docker Instance
 resource "aws_instance" "docker_instance" {
-  ami                    = "ami-0ebfd941bbafe70c6"
+  ami                    = "ami-053a862cc72bed182"
   instance_type          = var.docker_instance
   vpc_security_group_ids = [aws_security_group.docker_sg.id]
 
