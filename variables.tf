21a9725148c8393f4ecc8710bf46f5ef5db8afdd
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-01-30 22:19:02 +0200

    repo name

--- variables.tf
+++ variables.tf
@@ -37,6 +37,7 @@ variable "environment" {
 variable "ecr_repo_name" {
   description = "ECR Repo Name"
   type        = string
+  default     = docker_ecr_repo
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
