cbbab1cfcaf7367186848bebd17f18a111164054
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-01-30 22:37:59 +0200

    fix bucket

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_crypto_conf" {
-  bucket = aws_s3_bucket.bucket
+  bucket = aws_s3_bucket.bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
