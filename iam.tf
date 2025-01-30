9edd65d032191c437404cf712fce36c317828341
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-01-31 01:19:46 +0200

    resource

--- iam.tf
+++ iam.tf
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy" "ecr-tf-docker-role-policy" {
                 "ecr:PutImage"
             ],
             "Resource": [
-              "arn:aws:ecr:eu-north-1:992382655760:repository/docker_ecr_repo/*"
+              "*"
             ]
         }
     ]
