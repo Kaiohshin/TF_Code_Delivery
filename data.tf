affdad125501679c0bc0b7e2475a65e666e1b0e9
msurducan <msurducan@haventech.us>
2025-01-31 15:51:17 +0200

    name_value

--- data.tf
+++ data.tf
@@ -2,6 +2,6 @@ data "template_file" "docker-compose" {
   template = file("docker-compose.tpl")
 }
 
-data "aws_ssm_parameters_by_path" "ami" {
-  path = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+data "aws_ssm_parameter" "my-amzn-linux-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
