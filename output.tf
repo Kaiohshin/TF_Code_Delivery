8f2a1407935bcb0d51ac808c9fc5633318a89d7c
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-03 14:41:52 +0200

    4

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "instance" {
   value = aws_instance.docker_instance.public_dns
 }
 
-output "repository_arn" {
-  value = aws_ecr_repository.docker_ecr_repo.arn
-}
-
 output "dynamodb_table_arn" {
   value = module.dynamodb_table.dynamodb_table_arn
 }
