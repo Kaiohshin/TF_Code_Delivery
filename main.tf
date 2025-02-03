a155d3dd76ee1a9316c4f53221f2762edcf6ea52
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-03 18:48:45 +0200

    target_group_arns

--- main.tf
+++ main.tf
@@ -87,16 +87,16 @@ module "asg" {
   # Autoscaling group
   name = "${var.environment.name}-instance"
 
-  min_size                            = 0
-  max_size                            = 1
-  desired_capacity                    = 1
-  wait_for_capacity_timeout           = 0
-  health_check_type                   = "EC2"
-  vpc_zone_identifier                 = module.docker_vpc.public_subnets
-  autoscaling_group_target_group_arns = module.alb.autoscaling_group_target_group_arns
-  security_groups                     = [aws_security_group.docker_sg.id]
-  image_id                            = data.aws_ssm_parameter.my_amzn_linux_ami.value
-  instance_type                       = var.docker_instance
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.docker_vpc.public_subnets
+  target_group_arns         = module.alb.target_group_arns
+  security_groups           = [aws_security_group.docker_sg.id]
+  image_id                  = data.aws_ssm_parameter.my_amzn_linux_ami.value
+  instance_type             = var.docker_instance
 
   iam_instance_profile_name = aws_iam_instance_profile.tf_docker_role.name
 
