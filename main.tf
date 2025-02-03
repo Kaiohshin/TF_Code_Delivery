da13a1fbd947ce1d444acce583dd63867ccf02d7
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-03 17:37:50 +0200

    "

--- main.tf
+++ main.tf
@@ -160,23 +160,23 @@ module "alb" {
 
 module "cloudinit" {
   source  = "tedivm/cloudinit/general"
-  version = "~> 1.0
+  version = "~> 1.0"
 
   runcmd = [
-     "sudo yum update -y"
-  "sudo yum install -y docker"
-  "sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"
-  "sudo chmod +x /usr/local/bin/docker-compose"
-  "sudo systemctl start docker"
-  "sudo systemctl enable docker"
-  "sudo usermod -a -G docker ec2-user ""
-  "sudo chkconfig docker on"
-  "sudo chown $(whoami):$(whoami) /var/run/docker.sock"
-  "aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 992382655760.dkr.ecr.eu-north-1.amazonaws.com"
-  "docker-compose -f /opt/docker-compose.yml up -d"
+    "sudo yum update -y",
+    "sudo yum install -y docker",
+    "sudo curl -L ("https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)") -o /usr/local/bin/docker-compose",
+    "sudo chmod +x /usr/local/bin/docker-compose",
+    "sudo systemctl start docker",
+    "sudo systemctl enable docker",
+    "sudo usermod -a -G docker ec2-user ",
+    "sudo chkconfig docker on",
+    "sudo chown $(whoami):$(whoami) /var/run/docker.sock",
+    "aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 992382655760.dkr.ecr.eu-north-1.amazonaws.com",
+    "docker-compose -f /opt/docker-compose.yml up -d"
   ]
 
   config_files = {
-    "/opt/docker-compose.yml" : templatefile("${path.module}/docker-compose.yml", local.template_vars),
+    "/opt/docker-compose.yml" : templatefile("${path.module}/docker-compose.yml"),
   }
 }
