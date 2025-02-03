ceda7f3f9710da9a323abc7ee96d2e303160b9c5
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-03 16:41:32 +0200

    no default vpc id

--- main.tf
+++ main.tf
@@ -112,13 +112,13 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id          = module.docker_vpc.default_vpc_id
+  vpc_id          = module.docker_vpc.vpc_id
   subnets         = module.docker_vpc.public_subnets
   security_groups = [aws_security_group.docker_sg.id]
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
