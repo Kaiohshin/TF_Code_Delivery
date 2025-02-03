7f2d8e1432efaa504790854c5af5cd2b9f35e7ce
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-03 15:30:13 +0200

    repository_lifecycle_policy

--- main.tf
+++ main.tf
@@ -18,6 +18,25 @@ module "docker_ecr_repo" {
   source = "terraform-aws-modules/ecr/aws"
 
   repository_name = var.ecr_repo_name
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 3 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 3
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
   tags = {
     Terraform   = "true"
     Environment = "docker"
