488f5f6af9e378833d153c78e880d99edc2f34a4
msurducan <msurducan@haventech.us>
2025-02-05 17:59:54 +0200

    .id

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+   backend "remote" {
+    organization = "Dej"
+
+    workspaces {
+      name = "TF_Code_Delivery"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
