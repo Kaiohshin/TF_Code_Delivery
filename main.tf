2ff9fc363dbcac0cdf53d72d9c9be6a88e832c85
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-06 22:26:37 +0200

    map_public_ip_on_launch = true

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs                     = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ssm_parameter.my_amzn_linux_ami.value
-  instance_type = var.docker_instance
-  subnet_id     = module.blog_vpc.public_subnets[0]
-  # vpc_security_group_ids = default
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 # # EC2 Instance
 # resource "aws_instance" "docker_instance" {
 #   ami                    = data.aws_ssm_parameter.my_amzn_linux_ami.value
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 module "docker_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name           = var.environment.name
-  cidr           = var.cidr
-  azs            = var.azs
-  public_subnets = var.public_subnets
-  create_igw = true
-  
+  name                    = var.environment.name
+  cidr                    = var.cidr
+  azs                     = var.azs
+  public_subnets          = var.public_subnets
+  map_public_ip_on_launch = true
+
   tags = {
     Terraform   = "true"
     Environment = var.environment.name
