a72bf02c08a0c972405f095a84d013bdacdd6d4a
Kaiohshin <Kaioh_shin2007@yahoo.com>
2025-02-06 22:34:07 +0200

    ${base64encode

--- asg.tf
+++ asg.tf
@@ -18,7 +18,7 @@ module "asg" {
 
   iam_instance_profile_name = aws_iam_instance_profile.tf_docker_role.name
 
-  user_data = data.template_file.docker_compose.rendered
+  user_data = "${base64encode(data.template_file.docker_compose.rendered)}"
 #   user_data = module.cloudinit.rendered
   
 
